f4857d0592e1382a3fc9318d36b9e8ca8d5033a6
Lina Belitova <libra28@ya.ru>
2023-01-09 21:28:59 +0300

    Fix: cors check

--- backend/app.js
+++ backend/app.js
@@ -28,18 +28,14 @@ app.use(requestLogger);
 app.use((req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers["access-control-request-headers"];
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   if (method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
     res.header("Access-Control-Allow-Headers", requestHeaders);
     return res.end();
-  }
-  next();
-});
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
   }
   next();
 });
