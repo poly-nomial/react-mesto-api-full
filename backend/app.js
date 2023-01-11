4595f0cc743836eb1e50a233660e6605c45d7966
Lina Belitova <libra28@ya.ru>
2023-01-11 17:52:34 +0300

    cors?

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,14 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT, POST, GET, DELETE, OPTIONS, PATCH"
+    );
   }
   if (method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
